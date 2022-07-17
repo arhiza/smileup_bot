724550a9e1259b6fd6dd4229babad8e78e049a60
Tany Butor <arhiza@gmail.com>
2022-07-17 00:42:49 +0000

    stats

--- smileup/management/commands/send_to_all.py
+++ smileup/management/commands/send_to_all.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from smileup.models import Bot_info, BotUser, Post
 from smileup.bot import send_messages, reply_to_sender
 import telebot
+from django.utils import timezone
 
 
 class Command(BaseCommand):
@@ -11,10 +12,13 @@ class Command(BaseCommand):
         if bot_info:
             count_active_users = BotUser.objects.filter(dialog=BotUser.IDLE).count()
             count_not_active_users = BotUser.objects.exclude(dialog=BotUser.IDLE).count()
+            count_line = BotUser.objects.filter(next_message__lte=timezone.now()).filter(dialog=BotUser.IDLE).count()
             count_new_posts = Post.objects.filter(status=Post.NEW).count()
             count_edit_posts = Post.objects.filter(status=Post.EDIT).count()
 
-            mes_stat = f"<code>СТАТИСТИКА:</code> активных/неактивных пользователей - {count_active_users}/{count_not_active_users}, новых/отредактированных записей - {count_new_posts}/{count_edit_posts}"
+            mes_stat = f"<code>СТАТИСТИКА:</code> активных/неактивных пользователей - {count_active_users}/{count_not_active_users}, "+\
+                       f"в очереди на отправку - {count_line}, "+\
+                       f"новых/отредактированных записей - {count_new_posts}/{count_edit_posts}"
             bot = telebot.TeleBot(bot_info.token, threaded=False)
             reply_to_sender(bot_info.id_telegram_owner, mes_stat, bot)
 
